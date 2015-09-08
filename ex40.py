ee2cfdb40da4c7029e9a512d09750cffa02a24e7
Ryan Fry <rfry@airbud.com>
2015-09-08 12:46:24 -0700

    added a few more assignments about functions

--- ex40.py
+++ ex40.py
@@ -1,78 +1,25 @@
-import random
+class Song(object):
 
-player_cash = 100
-deck_of_cards = []
-player_hand = []
-dealer_hand = []
-player_hand_value = 0
-
-def suit_name(suit):
-	if suit == 0:
-		sname = 'Clubs'
-	elif suit == 1:
-		sname = 'Spades'
-	elif suit == 2:
-		sname = 'Hearts'
-	elif suit == 3:
-		sname = "Diamonds"
+	def __init__(self, lyrics):
+		self.lyrics = lyrics
 		
-	return sname
-	
-def card_name(number):
-	if number <= 10 and number > 1:
-		return number
-	elif number == 11:
-		return ('J')
-	elif number == 12:
-		return ('Q')
-	elif number == 13:
-		return ('K')
-	elif number == 1:
-		return ('A')
+	def sing_me_a_song(self):
+		for line in self.lyrics:
+			print line
+			
+happy_bday = Song(["Happy birthday to you",
+					"I dont want to get sued",
+					"So I'll stop right there"])
+					
+bulls_on_parade = Song(["They rally around your family",
+						"With a pocket full of shells"])
+						
+shake_ya_ass = Song(["Shake ya ass!",
+						"Watch ya self!",
+						"Shake ya ass!",
+						"Show me whatcha workin with!"])
+						
+shake_ya_ass.sing_me_a_song()						
+happy_bday.sing_me_a_song()
 
-def makedeck():
-	suit = 0
-	for i in range(0,4):
-		next_card = 0
-		for i in range(0,13):
-			next_card += 1
-			deck_of_cards.append("%s of %s" % (card_name(next_card),suit_name(suit)))
-		suit += 1
-	
-def shuffledeck():
-	random.shuffle(deck_of_cards)
-
-def displaydeck():
-	for i in deck_of_cards:
-		print i;
-
-def drawcard(towhom, numberofcards):
-	while(numberofcards > 0):
-		nextcard = deck_of_cards.pop(0)
-		numberofcards -= 1
-		if towhom == 'player':
-			player_hand.append(nextcard)
-		else:
-			dealer_hand.append(nextcard)
-
-def gamerounds():
-	endround = 0
-	while endround == 0:
-		makedeck()
-		shuffledeck()
-		drawcard('player', 2)
-		drawcard('dealer', 2)
-		print "the player has %s" %player_hand
-		print "the dealer has %s" %dealer_hand
-		hitorstay()
-		endround = 1;
-
-def hitorstay():
-	choice = raw_input("Do you want to hit or stay?")
-	if choice == 'hit'or choice == '1' or choice == 'Hit':
-		drawcard('player',1)
-		print player_hand
-		hitorstay()
-	else:
-		print "You stay with %s" % player_hand
-gamerounds()
+bulls_on_parade.sing_me_a_song()
